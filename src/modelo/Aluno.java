21407b5c368555fb36f490d256df50512cd8003e
Carolina Ribeiro <36118052018.1n@36DE194587.senacrjedu.local>
2018-09-26 19:48:10 -0300

    Adicao do arquivo gitgnore e atualizacao da classe Aluno

--- src/modelo/Aluno.java
+++ src/modelo/Aluno.java
@@ -5,6 +5,33 @@ public class Aluno {
 	private int matricula;
 	private String nome;
 	private String cpf;
+	private String email;
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 	
 	
 }
